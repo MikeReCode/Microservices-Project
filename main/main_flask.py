30b8755c6ab70608d81b79b02e7dfa38ff4167a4
MikeReCode <mihail.radu.work@gmail.com>
2023-05-16 17:59:41 +0300

    Implement endpoints

--- main/main_flask.py
+++ main/main_flask.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from sqlalchemy import UniqueConstraint
 import os
 import json
-
+from dataclasses import dataclass
+import requests
+from producer import publish
 
 app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://{str(os.getenv("MYSQL_USER"))}:{str(os.getenv("MYSQL_PASSWORD"))}@{str(os.getenv("MYSQL_HOST"))}/{str(os.getenv("MYSQL_DB"))}'
@@ -19,29 +21,42 @@ migrate = Migrate(app, db)
 # flask --app main_flask db migrate
 # flask --app main_flask db upgrade
 
-
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
+@dataclass
 class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
 
-    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
+    __table_args__ =(UniqueConstraint('user_id', 'product_id', name='user_product_unique'),)
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-    return f'mysql://{str(os.getenv("MYSQL_USER"))}:{str(os.getenv("MYSQL_PASSWORD"))}@{str(os.getenv("MYSQL_HOST"))}/{str(os.getenv("MYSQL_DB"))}'
+    return jsonify(Product.query.all())
 
-@app.route('/todo/')
-def todo():
-    product = Product.query.all()
-    print(product)
-    print(len(product))
-    return "hello"
+@app.route('/api/products/<int:id>/like', methods=["POST"])
+def like(id):
+    req = requests.get("http://docker.for.mac.localhost:8000/api/user")
+    json_req = req.json()
+
+    try:
+        productUser = ProductUser(user_id=json_req["id"], product_id=id)
+        db.session.add(productUser)
+        db.session.commit()
+        publish("product_liked", id)
+
+    except:
+        abort(400, "You already liked this product")
+    return jsonify({"message": "success"})
 
 
 if __name__ == "__main__":
